fb7cf9b785f3ba8877d3bdbd5987aec4ae9bab3a
abdulhakim2902 <hakim@blocksphere.id>
2022-07-01 13:23:39 +0700

    feat: implement claimTip and claimReference

--- near/src/tips_balance/internal.rs
+++ near/src/tips_balance/internal.rs
@@ -1,6 +1,24 @@
 use crate::*;
 
 impl TippingContract {
+	pub(crate) fn transfer(
+		&mut self,
+		receiver: &AccountId,
+		ft_identifier: &str,
+		amount: Balance,
+	) -> Promise {
+		if ft_identifier == "native" {
+			Promise::new(receiver.clone()).transfer(amount)
+		} else {
+			let ft_identifier = ft_identifier.parse::<AccountId>().unwrap();
+			ft_contract::ext(ft_identifier).with_attached_deposit(ONE_YOCTO).ft_transfer(
+				receiver.clone(),
+				U128(amount),
+				None,
+			)
+		}
+	}
+
 	pub(crate) fn create_tips_balance(
 		&mut self,
 		tips_balance_info: &TipsBalanceInfo,
@@ -21,43 +39,129 @@ impl TippingContract {
 
 	pub(crate) fn claim_tips_balance(
 		&mut self,
+		secondary_key: &TipsBalanceKey,
+		main_balance: &TipsBalance,
+	) -> TipsBalance {
+		let other_tips_balance = self.tips_balances.get(secondary_key);
+
+		if let Some(tips_balance) = other_tips_balance {
+			if !tips_balance.get_amount().is_zero() {
+				let tips_balance = tips_balance.set_balance(Zero::zero());
+				self.tips_balances.insert(secondary_key, &tips_balance);
+			}
+		}
+
+		let key = main_balance.key();
+		let reference_type = main_balance.get_reference_type();
+		let reference_id = main_balance.get_reference_id();
+
+		self.tips_balances.insert(&key, main_balance);
+		self.claim_reference_log(reference_type, reference_id);
+
+		main_balance.clone()
+	}
+
+	pub(crate) fn batch_claim_tips_balance(
+		&mut self,
+		secondary_keys: &Vec<TipsBalanceKey>,
+		main_balances: &Vec<TipsBalance>,
+	) {
+		for secondary_key in secondary_keys {
+			let other_tips_balance = self.tips_balances.get(secondary_key);
+
+			if let Some(tips_balance) = other_tips_balance {
+				if !tips_balance.get_amount().is_zero() {
+					let tips_balance = tips_balance.set_balance(Zero::zero());
+					self.tips_balances.insert(secondary_key, &tips_balance);
+				}
+			}
+		}
+
+		for main_balance in main_balances {
+			let key = main_balance.key();
+			let reference_type = main_balance.get_reference_type();
+			let reference_id = main_balance.get_reference_id();
+
+			self.tips_balances.insert(&key, main_balance);
+			self.claim_reference_log(reference_type, reference_id);
+		}
+	}
+
+	pub(crate) fn calculate_tips(
+		&mut self,
 		tips_balance_info: &TipsBalanceInfo,
 		reference_type: &ReferenceType,
 		reference_id: &ReferenceId,
-		account_id: &AccountId,
 	) -> TipsBalance {
 		let mut key = tips_balance_info.key();
+		let mut tip = Zero::zero();
 
-		let tips_balance = if tips_balance_info.get_reference_type() == reference_type {
-			assert!(tips_balance_info.get_reference_id() == reference_id, "WrongFormat");
+		if let Some(tips_balance) = self.tips_balances.get(&key) {
+			tip += tips_balance.get_amount();
+		}
 
-			self.tips_balances
-				.get(&key)
-				.unwrap_or_else(|| TipsBalance::new(tips_balance_info))
-		} else {
-			let mut tip = Zero::zero();
+		key.set_reference(reference_type, reference_id);
 
-			if let Some(tips_balance) = self.tips_balances.get(&key) {
-				tip += tips_balance.get_amount();
-				self.tips_balances.remove(&key);
+		match self.tips_balances.get(&key) {
+			Some(res) => res.add_balance(tip),
+			None => TipsBalance::new(tips_balance_info)
+				.set_balance(tip)
+				.set_reference(reference_type, reference_id),
+		}
+	}
+
+	pub(crate) fn batch_calculate_tips(
+		&mut self,
+		reference_type: &ReferenceType,
+		reference_ids: &Vec<ReferenceId>,
+		main_ref_type: &ReferenceType,
+		main_ref_id: &ReferenceId,
+		account_id: &AccountId,
+		tx_fee: Balance,
+	) -> (Vec<TipsBalance>, Vec<TipsBalanceKey>) {
+		let server_id = env::signer_account_id();
+		let values = self.ft_identifiers.values_as_vector();
+		let ft_infos: Vec<FtIdentifierInfo> =
+			(0..self.ft_identifiers.len()).filter_map(|index| values.get(index)).collect();
+
+		let mut tips_balances = Vec::<TipsBalance>::new();
+		let mut keys = Vec::<TipsBalanceKey>::new();
+
+		for ft_info in ft_infos.iter() {
+			let mut tip: Balance = Zero::zero();
+			let ft_id = ft_info.get_ft_identifier();
+			for reference_id in reference_ids {
+				let key = TipsBalanceKey::new(&server_id, reference_type, reference_id, ft_id);
+				if let Some(tips_balance) = self.tips_balances.get(&key) {
+					if !tips_balance.get_amount().is_zero() {
+						keys.push(key);
+						tip += tips_balance.get_amount();
+					}
+				}
 			}
 
-			key.set_reference(reference_type, reference_id);
-
-			match self.tips_balances.get(&key) {
+			let key = TipsBalanceKey::new(&server_id, main_ref_type, main_ref_id, ft_id);
+			let main_tips_balance = match self.tips_balances.get(&key) {
 				Some(res) => res.add_balance(tip),
-				None => TipsBalance::new(tips_balance_info)
-					.set_balance(tip)
-					.set_reference(reference_type, reference_id),
-			}
-		};
+				None => {
+					let tips_balance_info =
+						TipsBalanceInfo::new(&server_id, main_ref_type, main_ref_id, ft_id);
 
-		let tips_balance = tips_balance.set_account_id(account_id);
+					TipsBalance::new(&tips_balance_info).set_balance(tip)
+				},
+			};
 
-		self.tips_balances.insert(&key, &tips_balance);
-		self.claim_reference_log(reference_type, reference_id);
+			let main_tips_balance = if ft_id == "native" {
+				let amount = main_tips_balance.get_amount();
+				main_tips_balance.set_balance(amount - tx_fee)
+			} else {
+				main_tips_balance
+			};
 
-		tips_balance
+			tips_balances.push(main_tips_balance.set_account_id(account_id));
+		}
+
+		(tips_balances, keys)
 	}
 
 	pub(crate) fn internal_resolve_send_tip(
@@ -111,29 +215,59 @@ impl TippingContract {
 		}
 	}
 
+	pub(crate) fn internal_resolve_batch_claim_tip(&mut self, tips_balances: Vec<TipsBalance>) {
+		require!(env::promise_results_count() == tips_balances.len() as u64);
+
+		for tips_balance in tips_balances {
+			let key = tips_balance.key();
+			let amount = tips_balance.get_amount();
+			let tips_balance = tips_balance.set_balance(Zero::zero());
+			let ft_identifier = tips_balance.get_ft_identifier().to_string();
+
+			if let Some(ft_info) = self.ft_identifiers.get(&ft_identifier) {
+				let symbol = ft_info.get_symbol();
+				let decimals = ft_info.get_decimals() as usize;
+				let receiver = tips_balance.get_account_id().clone().unwrap();
+				let amount_str = amount.to_string();
+
+				self.tip_log(&receiver, &amount_str, symbol, decimals, None);
+			}
+
+			self.tips_balances.insert(&key, &tips_balance);
+		}
+	}
+
 	pub(crate) fn internal_resolve_claim_reference(
 		&mut self,
-		tips_balance_info: TipsBalanceInfo,
-		reference_type: ReferenceType,
-		reference_id: ReferenceId,
-		account_id: AccountId,
+		secondary_key: TipsBalanceKey,
+		main_balance: TipsBalance,
+		native_tips_balance: Option<TipsBalance>,
 	) {
 		require!(env::promise_results_count() == 1);
 
 		match env::promise_result(0) {
-			PromiseResult::Successful(val) => {
-				let ft_metadata = from_slice::<FungibleTokenMetadata>(&val).expect("TokenNotFound");
-				let symbol = ft_metadata.get_symbol();
-				let decimals = ft_metadata.get_decimals();
-				let ft_identifier = tips_balance_info.get_ft_identifier().to_string();
+			PromiseResult::Successful(_) => {
+				if let Some(native) = native_tips_balance {
+					let native_key = native.key();
+					self.tips_balances.insert(&native_key, &native);
+				}
 
-				self.create_ft_identifier(&ft_identifier, symbol, decimals);
-				self.claim_tips_balance(
-					&tips_balance_info,
-					&reference_type,
-					&reference_id,
-					&account_id,
-				);
+				self.claim_tips_balance(&secondary_key, &main_balance);
+			},
+			_ => env::panic_str("TokenNotFound"),
+		};
+	}
+
+	pub(crate) fn internal_resolve_batch_claim_reference(
+		&mut self,
+		secondary_keys: Vec<TipsBalanceKey>,
+		main_balances: Vec<TipsBalance>,
+	) {
+		require!(env::promise_results_count() == 1);
+
+		match env::promise_result(0) {
+			PromiseResult::Successful(_) => {
+				self.batch_claim_tips_balance(&secondary_keys, &main_balances);
 			},
 			_ => env::panic_str("TokenNotFound"),
 		};
--- near/src/tips_balance/tests.rs
+++ near/src/tips_balance/tests.rs
@@ -24,7 +24,7 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
+		let tips_balance_info = TipsBalanceInfo::new(&accounts(4), "user", "user_id", "native");
 		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
 
 		// Send tip to user-user_id
@@ -44,100 +44,6 @@ mod tips_balance_tests {
 	}
 
 	#[test]
-	fn claim_reference_works() {
-		// Initialize contract
-		let mut context = get_context(accounts(0));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info_1 = TipsBalanceInfo::new("myriad", "people", "people_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info_1.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-		let tips_balance_info_2 = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
-
-		// Claim reference
-		testing_env!(context.predecessor_account_id(accounts(0)).attached_deposit(1).build());
-		contract.claim_reference(
-			tips_balance_info_1.clone(),
-			reference_type,
-			reference_id,
-			accounts(5),
-		);
-
-		// Test
-		let tips_balance = contract.get_tips_balance(tips_balance_info_1);
-		assert_eq!(tips_balance, None);
-		let tips_balance = TipsBalance::new(&tips_balance_info_2).set_balance(tip);
-
-		let expected_tips_balance = tips_balance.set_account_id(&accounts(5));
-		let tips_balance = contract.get_tips_balance(tips_balance_info_2).unwrap();
-		let formatted_tips_balance = TipsBalanceWithFormattedBalance {
-			tips_balance: expected_tips_balance,
-			symbol: String::from("NEAR"),
-			formatted_amount: String::from("0.1"),
-		};
-
-		assert_eq!(formatted_tips_balance, tips_balance);
-	}
-
-	#[test]
-	fn claim_tips_works() {
-		// Initialize contract
-		let mut context = get_context(accounts(0));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info_1 = TipsBalanceInfo::new("myriad", "people", "people_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info_1.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-		let tips_balance_info_2 = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
-
-		// Claim reference
-		testing_env!(context.signer_account_id(accounts(0)).attached_deposit(1).build());
-		contract.claim_reference(
-			tips_balance_info_1.clone(),
-			reference_type,
-			reference_id,
-			accounts(4),
-		);
-
-		// Claim tip
-		testing_env!(context.signer_account_id(accounts(4)).attached_deposit(1).build());
-		contract.claim_tip(tips_balance_info_2.clone());
-
-		// Test
-		let tips_balance = contract.get_tips_balance(tips_balance_info_1);
-		assert_eq!(tips_balance, None);
-		let tips_balance = TipsBalance::new(&tips_balance_info_2);
-
-		let expected_tips_balance = tips_balance.set_account_id(&accounts(4));
-		let tips_balance = contract.get_tips_balance(tips_balance_info_2).unwrap();
-		let formatted_tips_balance = TipsBalanceWithFormattedBalance {
-			tips_balance: expected_tips_balance,
-			symbol: String::from("NEAR"),
-			formatted_amount: String::from("0"),
-		};
-
-		assert_eq!(formatted_tips_balance, tips_balance);
-	}
-
-	#[test]
 	#[should_panic(expected = "OnlyForNear")]
 	fn cant_send_tip_when_tipping_with_fungible_token() {
 		// Initialize contract
@@ -146,7 +52,8 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "fungible_token");
+		let tips_balance_info =
+			TipsBalanceInfo::new(&accounts(4), "user", "user_id", "fungible_token");
 
 		// Send tip to user-user_id
 		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(0).build());
@@ -162,7 +69,7 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
+		let tips_balance_info = TipsBalanceInfo::new(&accounts(4), "user", "user_id", "native");
 
 		// Send tip to user-user_id
 		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(0).build());
@@ -178,7 +85,7 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
+		let tips_balance_info = TipsBalanceInfo::new(&accounts(4), "user", "user_id", "native");
 
 		// Send tip to user-user_id
 		testing_env!(context
@@ -190,107 +97,6 @@ mod tips_balance_tests {
 	}
 
 	#[test]
-	#[should_panic(expected = "Requires attached deposit of exactly 1 yoctoNEAR")]
-	fn cant_claim_reference_without_min_attached_deposit() {
-		// Initialize contract
-		let mut context = get_context(accounts(1));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "people", "people_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-
-		// Claim reference
-		contract.claim_reference(tips_balance_info, reference_type, reference_id, accounts(5));
-	}
-
-	#[test]
-	#[should_panic(expected = "Unauthorized")]
-	fn cant_claim_reference_when_unauthorized() {
-		// Initialize contract
-		let mut context = get_context(accounts(1));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "people", "people_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-
-		// Claim reference
-		testing_env!(context.signer_account_id(accounts(3)).attached_deposit(1).build());
-		contract.claim_reference(tips_balance_info, reference_type, reference_id, accounts(5));
-	}
-
-	#[test]
-	#[should_panic(expected = "WrongFormat")]
-	fn cant_claim_reference_when_reference_id_not_same() {
-		// Initialize contract
-		let mut context = get_context(accounts(0));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_idd");
-
-		// Claim reference
-		testing_env!(context.predecessor_account_id(accounts(0)).attached_deposit(1).build());
-		contract.claim_reference(tips_balance_info, reference_type, reference_id, accounts(4));
-	}
-
-	#[test]
-	#[should_panic(expected = "NothingToClaimed")]
-	fn cant_claim_tips_when_nothing_to_claimed() {
-		// Initialize contract
-		let mut context = get_context(accounts(0));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
-
-		// Claim reference
-		testing_env!(context.predecessor_account_id(accounts(0)).attached_deposit(1).build());
-		contract.claim_reference(
-			tips_balance_info.clone(),
-			reference_type,
-			reference_id,
-			accounts(4),
-		);
-
-		// Claim tip
-		testing_env!(context.predecessor_account_id(accounts(4)).attached_deposit(1).build());
-		contract.claim_tip(tips_balance_info);
-	}
-
-	#[test]
 	#[should_panic(expected = "ReceiverNotExists")]
 	fn cant_claim_tips_when_receiver_not_exists() {
 		// Initialize contract
@@ -299,7 +105,7 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info_1 = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
+		let tips_balance_info_1 = TipsBalanceInfo::new(&accounts(4), "user", "user_id", "native");
 		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
 
 		// Send tip to user-user_id
@@ -312,40 +118,6 @@ mod tips_balance_tests {
 	}
 
 	#[test]
-	#[should_panic(expected = "Unauthorized")]
-	fn cant_claim_tips_when_unauthorized() {
-		// Initialize contract
-		let mut context = get_context(accounts(0));
-		testing_env!(context.build());
-		let mut contract = TippingContract::new(None);
-
-		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
-		let tip: Balance = 100000000000000000000000; // 0.1 NEAR
-
-		// Send tip to user-user_id
-		testing_env!(context.predecessor_account_id(accounts(3)).attached_deposit(tip).build());
-		contract.send_tip(tips_balance_info.clone());
-
-		// Payload
-		let reference_type = String::from("user");
-		let reference_id = String::from("user_id");
-
-		// Claim reference
-		testing_env!(context.predecessor_account_id(accounts(0)).attached_deposit(1).build());
-		contract.claim_reference(
-			tips_balance_info.clone(),
-			reference_type,
-			reference_id,
-			accounts(4),
-		);
-
-		// Claim tip
-		testing_env!(context.predecessor_account_id(accounts(5)).attached_deposit(1).build());
-		contract.claim_tip(tips_balance_info);
-	}
-
-	#[test]
 	#[should_panic(expected = "TipsBalanceNotExists")]
 	fn cant_claim_tips_when_data_not_exists() {
 		// Initialize contract
@@ -354,7 +126,7 @@ mod tips_balance_tests {
 		let mut contract = TippingContract::new(None);
 
 		// Payload
-		let tips_balance_info = TipsBalanceInfo::new("myriad", "user", "user_id", "native");
+		let tips_balance_info = TipsBalanceInfo::new(&accounts(4), "user", "user_id", "native");
 
 		// Claim tip
 		testing_env!(context.predecessor_account_id(accounts(5)).attached_deposit(1).build());
