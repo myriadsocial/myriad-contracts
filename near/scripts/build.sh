60f03f940bb45bba2e43e3a9ebc80bf31217f0e4
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2022-07-18 11:35:20 +0700

    feat: added deployment script (#7)
    * feat: added execute permission
    
    * feat: added deployment script

--- near/scripts/build.sh
+++ near/scripts/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-TARGET="${CARGO_TARGET_DIR:-target}"
+
 set -e
+
+pushd .
+
+# The following line ensure we run from the project root
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT/near/contracts/tipping
 
 cargo build --target wasm32-unknown-unknown --release
+
+TARGET="${CARGO_TARGET_DIR:-target}"
 cp $TARGET/wasm32-unknown-unknown/release/tipping.wasm ../../artifacts/
